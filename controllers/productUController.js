54a3772f4f81aab3129c8ea86038fc6dd08857be
thapanankulab25 <152828615+thapanankulab25@users.noreply.github.com>
2023-12-29 13:56:39 +0700

    update from

--- controllers/productUController.js
+++ controllers/productUController.js
@@ -1,8 +1,21 @@
-const User = require('../models/User')
-module.exports = async(req,res) =>{
+const User = require('../models/User');
+const Product = require('../models/Product'); // Assuming you have a Product model
 
-    let UserData = await User.findById(req.session.userId)
-    res.render('productU', {
-        UserData  
-    })
-}
+module.exports = async (req, res) => {
+    try {
+        // Get user data
+        let UserData = await User.findById(req.session.userId);
+
+        // Get all products
+        let products = await Product.find();
+
+        // Render the view with user data and products
+        res.render('productU', {
+            UserData,
+            products
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
